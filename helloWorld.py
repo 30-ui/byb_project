5838b0143b051a4cd4c9c91c9417417d773cba38
30-ui <sheryl.ffrench@gmail.com>
2025-01-12 18:33:21 +0000

    modified the message printed

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,6 @@
  # Python program to print "Hello, World!"
-greeting = "Hello, World!"
-print("Hello, World!")
+# greeting = "Hello, World!"
+# print("Hello, World!")
+greeting = "Git is Awesome!"
+print(greeting)
+
